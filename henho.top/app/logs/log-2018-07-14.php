ffa7b46cc18e61fe3e47ee01f95427f9e9b6ba0e
Tung Nguyen Trung <tung.nguyen.intern@ntq-solution.com.vn>
2018-09-08 13:05:03 +0700

    manager sale POS

---
+++ henho.top/app/logs/log-2018-07-14.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-14 14:29:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\xampp\htdocs\henho.top\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-14 14:29:05 --> Unable to connect to the database
+ERROR - 2018-07-14 14:29:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\xampp\htdocs\henho.top\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-14 14:29:05 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\henho.top\system\libraries\Session\Session_driver.php 205
+ERROR - 2018-07-14 14:29:05 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\henho.top\system\libraries\Session\Session.php 143
+ERROR - 2018-07-14 14:29:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\xampp\htdocs\henho.top\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-14 14:29:05 --> Unable to connect to the database
+ERROR - 2018-07-14 14:29:05 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2018-07-14 14:29:05 --> Severity: error --> Exception: Call to a member function num_rows() on boolean C:\xampp\htdocs\henho.top\app\models\Site.php 42
---
+++ henho.top/files/db_updates/POS.3.4.4.sql
@@ -0,0 +1,1 @@
+UPDATE `sma_pos_settings` SET `version` = '3.4.4' WHERE `pos_id` = 1;
